b26675eeb4605dc8b798d8f401e00a1a9fb383e0
taliban2299 <147526797+taliban2299@users.noreply.github.com>
2024-07-12 00:18:34 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,18 +3,18 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "254115758028"],
+global.ownername = process.env.OWNER_NAME || "💙LEWIS",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Kenyan"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "PROFESSOR X",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "💙LEWIS",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.author = process.env.AUTHOR || "💙LEWIS",
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
@@ -33,20 +33,20 @@ global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
 //reply messages
 global.mess = {
-    done: '🍭 *Here you go, darling!* ',
-    prem: '🍭 *My darling, this feature is reserved for premium users only*',
-    admin: '🍭 *My darling, this feature is reserved for admins only*',
-    botAdmin: '🍭 *Darling, this feature can only be used when the bot is a group admin* ',
-    owner: '🍭 *My dearest, this feature is reserved for the owner only*',
-    group: '🍭 *Sweetheart, this feature is exclusively for groups*',
-    private: '🍭 *My love, this feature is exclusively for private chats*',
-    wait: '🍭 *Darling, in process...* ',    
+    done: '🍭 *Here you go!* ',
+    prem: '🍭 *Oh dear, this feature is reserved for premium users only*',
+    admin: '🍭 *Oh dear, this feature is reserved for admins only*',
+    botAdmin: '🍭 *Dear, this feature can only be used when the bot is a group admin* ',
+    owner: '🍭 *Oh dear, this feature is reserved for the owner only*',
+    group: '🍭 *Yooh, this feature is exclusively for groups*',
+    private: '🍭 *Yooh, this feature is exclusively for private chats*',
+    wait: '🍭 *Chill, in process...* ',    
     error: '🍭 *Oh darling, there seems to be an error!*',
 }
 
